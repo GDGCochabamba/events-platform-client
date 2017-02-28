75d57dd776e934018c2b1790347bb916cf3b2156
Julio Silva <julio.sguz@gmail.com>
2017-02-27 22:16:46 -0400

    Add base colors for Angular Material

--- client/app/root.module.js
+++ client/app/root.module.js
@@ -1,4 +1,35 @@
 angular
   .module('eventsPlatformClient', [
-    'common'
-  ]);
+    'common',
+    'ngMaterial'
+  ])
+  .config(function($mdThemingProvider) {
+
+    var primaryPalette = $mdThemingProvider.extendPalette('blue', {
+      '400': '#0177c1'
+    });
+
+    var accentPalette = $mdThemingProvider.extendPalette('yellow', {
+      '400': '#ffc107'
+    });
+
+    var warnPalette = $mdThemingProvider.extendPalette('yellow', {
+      '400': '#ff6e6e'
+    });
+
+    $mdThemingProvider.definePalette('primaryPalette', primaryPalette);
+    $mdThemingProvider.definePalette('accentPalette', accentPalette);
+    $mdThemingProvider.definePalette('warnPalette', warnPalette);
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('primaryPalette', {
+        'default': '400'
+      })
+      .accentPalette('accentPalette', {
+        'default': '400'
+      })
+      .warnPalette('warnPalette', {
+        'default': '400'
+      });
+
+  });
